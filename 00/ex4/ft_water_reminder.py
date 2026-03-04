6d156c18b7013fd3ce1a1ff0bb5ac05f6dd1b42b
adaza-ru <adaza-ru@student.42malaga.com>
2026-03-04 01:56:49 +0100

    cambio 00

--- 00/ex4/ft_water_reminder.py
+++ 00/ex4/ft_water_reminder.py
@@ -7,12 +7,12 @@
 #   By: adaza-ru <adaza-ru@student.42malaga.com>     +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/02/16 18:17:07 by adaza-ru            #+#    #+#            #
-#   Updated: 2026/02/16 18:25:42 by adaza-ru           ###   ########.fr      #
+#   Updated: 2026/03/03 22:20:37 by adaza-ru           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 def ft_water_reminder():
-    days = int(input("Enter plant age in days: "))
+    days = int(input("Days since last watering: "))
     if days > 2:
         print("Water the plants!")
     else:
