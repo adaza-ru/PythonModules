0ed547eabd54b7b92098705580e632b584469b33
adaza-ru <adaza-ru@student.42malaga.com>
2026-03-12 17:54:59 +0100

    add: 01 ex3 modified with dictionary unpacking

--- 01/ex3/ft_plant_factory.py
+++ 01/ex3/ft_plant_factory.py
@@ -21,28 +21,28 @@ class Plant:
         days (int): The days of the plant in days.
     """
 
-    def __init__(self, name: str, height: int, days: int) -> None:
+    def __init__(self, name: str, height: int, age: int) -> None:
         """Initialize the Plant with its specific data."""
         self.name: str = name
         self.height: int = height
-        self.days: int = days
+        self.age: int = age
 
     def get_info(self) -> str:
         """Return a user-friendly string representation of the plant."""
-        return f"{self.name}: {self.height}cm, {self.days} days old"
+        return f"{self.name}: {self.height}cm, {self.age} days old"
 
     def grow(self, cm: int) -> None:
         """Increase the height of the plant."""
         self.height += cm
 
-    def age(self) -> None:
+    def days(self) -> None:
         """Increase the age of the plant by one day."""
-        self.days += 1
+        self.age += 1
 
 
 def main() -> None:
     """Streamlines plant creation by iterating over a data source."""
-    raw_data: list = [
+    raw_data: list[dict] = [
         {"name": "Rose", "height": 25, "age": 30},
         {"name": "Oak", "height": 200, "age": 365},
         {"name": "Cactus", "height": 5, "age": 90},
@@ -53,7 +53,7 @@ def main() -> None:
     print("=== Plant Factory Output ===")
 
     for d in raw_data:
-        new_plant: Plant = Plant(d["name"], d["height"], d["age"])
+        new_plant: Plant = Plant(**d)
         print(new_plant.get_info())
 
     print("\nTotal plants created: 5")
