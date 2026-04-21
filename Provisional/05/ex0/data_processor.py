834c4891c6a1840b81965ab91231c0d44d22bd4f
adaza-ru <adaza-ru@student.42malaga.com>
2026-04-21 16:27:15 +0200

    cambio 05/0

--- Provisional/05/ex0/data_processor.py
+++ Provisional/05/ex0/data_processor.py
@@ -46,32 +46,102 @@ class NumericProcessor(DataProcessor):
 
 class TextProcessor(DataProcessor):
 
-    pass
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, str):
+            return True
+        if isinstance(data, list):
+            return all(isinstance(x, str) for x in data)
+        return False
+
+    def ingest(self, data: Any) -> None:
+
+        if not self.validate(data):
+            raise ValueError("Improper text data")
+        if isinstance(data, list):
+            for item in data:
+                self._storage.append((self._current_rank, str(item)))
+                self._current_rank += 1
+        else:
+            self._storage.append((self._current_rank, str(data)))
+            self._current_rank += 1
 
 
 class LogProcessor(DataProcessor):
 
-    pass
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict):
+            return (
+                isinstance(data.get("log_level"), str) and
+                isinstance(data.get("log_message"), str)
+            )
+        if isinstance(data, list):
+            return all(
+                    isinstance(x, dict) and
+                    isinstance(x.get("log_level"), str) and
+                    isinstance(x.get("log_message"), str)
+                    for x in data
+                )
+        return False
+
+    def ingest(self, data: Any) -> None:
+
+        if not self.validate(data):
+            raise ValueError("Improper log data")
+        if isinstance(data, list):
+            for d in data:
+                self._storage.append(
+                        (self._current_rank,
+                         f"{k}: {v}") for k, v in d.items())
+                self._current_rank += 1
+        else:
+            self._storage.append(
+                (self._current_rank,
+                 f"{k}: {v}") for k, v in data.items())
+            self._current_rank += 1
 
 
 if __name__ == "__main__":
     print("=== Code Nexus - Data Processor ===")
 
     print("\nTesting Numeric Processor...")
-    print("Trying to validate input '42': ", end="")
+    print(" Trying to validate input '42': ", end="")
     print(NumericProcessor().validate(42))
-    print("Trying to validate input 'Hello': ", end="")
+    print(" Trying to validate input 'Hello': ", end="")
     print(NumericProcessor().validate("Hello"))
-    print("Test invalid ingestion of string 'foo' without prior validation:")
+    print(" Test invalid ingestion of string 'foo' without prior validation:")
     try:
         NumericProcessor().ingest("foo")
     except ValueError as e:
-        print(f"Got exception: {e}")
-    test_data: list[int] = [1, 2, 3, 4, 5]
-    print(f"Processing data: {test_data}")
+        print(f" Got exception: {e}")
+    test_numeric: list[int] = [1, 2, 3, 4, 5]
+    print(f" Processing data: {test_numeric}")
     np: NumericProcessor = NumericProcessor()
-    np.ingest(test_data)
-    print("Extracting 3 values...")
+    np.ingest(test_numeric)
+    print(" Extracting 3 values...")
     for _ in range(3):
         rank, value = np.output()
-        print(f"Numeric value {rank}: {value}")
+        print(f" Numeric value {rank}: {value}")
+
+    print("\nTesting Text Processor...")
+    print(" Trying to validate input '42': ", end="")
+    print(TextProcessor().validate(42))
+    test_text: list[str] = ["Hello", "Nexus", "World"]
+    print(f" Processing data: {test_text}")
+    tp: TextProcessor = TextProcessor()
+    tp.ingest(test_text)
+    print(" Extracting 1 value...")
+    for _ in range(1):
+        rank, value = tp.output()
+        print(f" Text value {rank}: {value}")
+
+    print("\nTesting Log Processor...")
+    print(" Trying to validate input 'Hello': ", end="")
+    print(LogProcessor().validate("Hello"))
+    test_log: list[dict] = [
+        {'log_level': 'NOTICE', 'log_message': 555},
+        {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}
+        ]
+    print(f" Processing data: {test_log}")
+    tl: LogProcessor = LogProcessor()
+    print(f"{tl.validate(test_log)}")
+
